c44ff97135e70c121f5716434cd5c5b12d413af4
Deep Bratt Mondal <deepbratt@gmail.com>
2019-08-29 20:04:24 +0530

    adsdsa

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var services = require('./controller/services');
 
 
 //showing services function
+//http://3.16.159.114:3000/services using forever npm
 services(app);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT);
-console.log('You app is live on http://localhost:' + PORT);
+console.log('You app is live on port:' + PORT);
