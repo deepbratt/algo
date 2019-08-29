fe1568c580fb4bdb66d611184fba8119ed287c42
Deep Bratt Mondal <deepbratt@gmail.com>
2019-08-29 18:01:01 +0530

    asdsad
    asdasd

--- controller/services.js
+++ controller/services.js
@@ -1,1 +1,11 @@
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
+require('../config/db.js');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+module.exports = function(app){
+  app.get('/services', function(req, res){
+    res.send('all services will be shown asap.');
+  });
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var services = require('./controller/services');
 
 
+//showing services function
+services(app);
 
 const PORT = process.env.PORT || 3000
-
 app.listen(PORT);
 console.log('You app is live on http://localhost:' + PORT);
