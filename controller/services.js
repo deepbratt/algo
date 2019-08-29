ab7d6b1e48b104a6d3fbcadc0939860491e13be9
Deep Bratt Mondal <deepbratt@gmail.com>
2019-08-29 18:08:56 +0530

    asdasd

--- controller/services.js
+++ controller/services.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
-require('../config/db.js');
+var db = require('../config/db.js');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 
 module.exports = function(app){
   app.get('/services', function(req, res){
-    res.send('all services will be shown asap.');
+    var PREPARING_BUNDLE_ALLDATA = "SELECT * FROM supplier_service limit 50";
+    db.query(PREPARING_BUNDLE_ALLDATA, function (err, result, fields) {
+      if (err) throw err;
+      res.send(JSON.stringify (result));
+    });
   });
 };
