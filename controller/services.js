643a2f116aa5752caeb27e5c20f966237fdd6b1e
Deep Bratt Mondal <deepbratt@gmail.com>
2019-08-31 14:02:05 +0530

    asdas

--- controller/services.js
+++ controller/services.js
@@ -1,4 +1,8 @@
+var stream = require('stream');
 var mysql = require('mysql');
+var http = require('http');
+var util = require('util');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var db = require('../config/db.js');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -6,10 +10,25 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
   app.get('/services', function(req, res){
-    var PREPARING_BUNDLE_ALLDATA = "SELECT * FROM supplier_service limit 50";
+    /*var PREPARING_BUNDLE_ALLDATA = "SELECT * FROM supplier_service limit 50";
     db.query(PREPARING_BUNDLE_ALLDATA, function (err, result, fields) {
       if (err) throw err;
       res.send(JSON.stringify (result));
-    });
-  });
+    });*/
+    db.query("SELECT * FROM supplier_service limit 50")
+      .stream()
+      .pipe(stream.Transform({
+        objectMode: true,
+        transform: function(data,encoding,callback) {
+          //res.write(util.inspect(JSON.stringify(data)));
+          res.write(util.inspect(JSON.stringify(data)));
+          console.log('new chunk received');
+          callback()
+        }
+       })
+       .on('finish',function() {console.log('done')})
+     )
+
+
+   });
 };
